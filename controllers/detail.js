aa8422ba327cef7d384f8762c79085adb348f603
VanXoan <vansoan20@gmail.com>
2023-04-01 07:02:08 +0700

    Call Api tư sever , cho xu li Css

--- controllers/detail.js
+++ controllers/detail.js
@@ -1,28 +1,56 @@
 
+function getProductDetail() {
+    //Lấy tham số từ url
 
-// console.log(window.location.href)
+    let param = new URLSearchParams(window.location.search);
+    const thamSo = param.get('id');// id 
 
-// function getProductDetail () {
-//     //Lấy tham số từ url
-//     var param = new URL(window.location.href);
+    let promise = axios({
+        url: `https://shop.cyberlearn.vn/api/Product/getbyid?id=${thamSo}`,
+        method: 'GET',
 
-//     var thamSo =param.searchParams.get('id')
+    });
 
+    promise.then(function (res) {
+        console.log('data', res.data.content);
 
+        let valid = -1;
+        var output = '';
+        for (let index in (res.data.content)) {
 
-//     var promise = axios({
-//         url: 'https://shop.cyberlearn.vn/api/Product/getbyid?=id',
-//         method:'GET'
-//     }) 
+            if ((res.data.content)[index] !== valid) {
+                let arr = res.data.content; // gán dữ liệu từ api về = arr cho nó ngắn + nhìn đỡ rối
+                output +=
+                    `
+                    <div>
+                    <div class="proItem-left">
+                        <img src="${arr.image}" alt="1">
+                    </div>
+                    <div class="proItem-right">
+                        <h2>Product name</h2>
+                        <p>${arr.description}</p>
+                        <h3>${arr.name}</h3>
+                        <div>
+                            ${arr.size}
+                        </div>
+                        <p>${arr.price}</p>
+                        <p>Phần add to cart này a làm dc thì làm k thì em làm luôn</p>
+                    </div>
+        
+                </div>
+            `
+                break;// tìm dc thì thoát ra k nó in ra 1 đống luôn
+            }
+        }
+        document.getElementById('productsDetail').innerHTML = output;
+    });
 
-//     promise.then(function(res){
-//         console.log('data',res.data.content);
-//         //Dom đến các phần trên giao diện để append vào
+    promise.catch(function (err) {
+        console.log(err);
+    });
+}
 
-//     })
+window.addEventListener('load', function () {
+    getProductDetail();
 
-  
-
-// }
-
-// getProductDetail();
+});
--- controllers/index.js
+++ controllers/index.js
@@ -12,21 +12,18 @@ function callAPIProducts() {
     var valid = -1;
     var output = '';
     for (let index of (res.data.content)) {//res.data.content :content là thuộc tính có tên content trong data
-  
+
       if (index !== valid) {
         output += `
         <div class=" col-12 col-md-6 col-lg-4" id="${index.id}">
         <div class="card">
-
-          <img src="${index.image}" class="card-img-top" alt="1">
-
+          <img src="${index.image}" class="card-img-top" alt="proIndex">
           <div class="card-body">
             <h2 class="card-title">${index.name}</h2>
             <p class="card-text">${index.shortDescription}</p>
           </div>
-
           <div class="card-footer">
-            <span class="buy-now"><a href="../detail.html">Buy now</a></span>
+            <span class="buy-now"><a href="../detail.html?id=${index.id}">Buy now</a></span>
             <span class="price">${index.price}$</span>
           </div>
 
@@ -39,17 +36,14 @@ function callAPIProducts() {
     }
 
     document.getElementById('productFeature').innerHTML = output;
-
   });
 
   promise.catch(function (err) {
     console.log(err);
-
   });
 
 }
 
 window.addEventListener('load', function () {
   callAPIProducts();
-
 });
